2f099078289fa621c7da9101bdc21e1b5456213f
DWA <dwacoin@gmail.com>
2021-10-22 10:48:44 +0700

    start script

--- start.sh
+++ start.sh
@@ -16,12 +16,13 @@ if [ -x jre/bin/java ]; then
 else
     JAVA=java
 fi
-unameOut="$(uname -s)"
-case "${unameOut}" in
-    Linux*)     MEM=`awk '/MemTotal/ { printf "%.0fm", $2*6/10000 }' /proc/meminfo`;;
-    Darwin*)    MEM=`system_profiler SPHardwareDataType | grep "Memory:" | awk '/Memory/ { printf "%.0fm", $2*1000000*6/10000 }'`;;
-    *)          MEM='1000m'
-esac
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        MEM=`awk '/MemTotal/ { printf "%.0fm", $2*6/10000 }' /proc/meminfo`;
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        MEM=`system_profiler SPHardwareDataType | grep "Memory:" | awk '/Memory/ { printf "%.0fm", $2*1000000*6/10000 }'`;
+else
+        MEM='1000m'
+fi
 nohup ${JAVA} -XX:MaxGCPauseMillis=50 -XX:NewRatio=2 -Xmx${MEM} -Xms${MEM} -cp classes:lib/*:conf:addons/classes:addons/lib/* -Ddwa.runtime.mode=desktop dwa.Dwa > /dev/null 2>&1 &
 echo $! > ~/.dwa/dwa.pid
 cd - > /dev/null
